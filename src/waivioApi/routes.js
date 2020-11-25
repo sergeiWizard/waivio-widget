2b5892f2ea844619d264b6febcabca560abfc8a8
Sergei <sergeij@wizardsdev.com>
2020-11-25 11:41:44 +0200

    CRA

---
+++ src/waivioApi/routes.js
@@ -0,0 +1,12 @@
+import config from './config.json';
+
+const baseUrl = config[process.env.NODE_ENV].protocol + config[process.env.NODE_ENV].host;
+
+const routes = {
+    ...config,
+    apiPrefix: baseUrl + config.apiPrefix,
+    objectsBotApiPrefix: baseUrl + config.objectsBotApiPrefix,
+    campaignApiPrefix: baseUrl + config.campaignApiPrefix,
+};
+
+export default routes;
